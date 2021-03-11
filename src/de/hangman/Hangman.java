7a7a6fc8e7d21a7227344a5e010f9fdc2d809a82
Jonaldo111 <79648072+Jonaldo111@users.noreply.github.com>
2021-03-11 15:54:39 +0100

    moin
    moin

--- src/de/hangman/Hangman.java
+++ src/de/hangman/Hangman.java
@@ -5,7 +5,9 @@ public class Hangman {
 	public static void main(String[] args) {
 		
 		System.out.print( "This is Hangman" );
-
+		moin
+		moin
+		
 	}
 
 }
---
+++ src/de/hangman/event/event.java
@@ -0,0 +1,10 @@
+package de.hangman.event;
+
+public class event {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
