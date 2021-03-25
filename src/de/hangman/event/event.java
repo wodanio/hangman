13470ed68abbb4f158cf6287895ad44d74873cd9
Jonaldo111 <79648072+Jonaldo111@users.noreply.github.com>
2021-03-25 14:57:56 +0100

    keine Zahlen

--- src/de/hangman/event/event.java
+++ src/de/hangman/event/event.java
@@ -7,15 +7,23 @@ public class event {
 	public event() {
 		
 		Scanner s = new Scanner(System.in);
-		String x;
+		int x;
 		
 	// Eingabe eines Buchstaben
 	System.out.println("Wähle einen Buchstaben");
 	
-	x = s.next();
+	x = s.nextInt();
 	
 	
-	System.out.println(x);
+	// Zahlen vermeiden
+	char letter = 'A';
+	String validChars = "abcdefghijklmnopqrstuvwxyz";
+	char[] validCharsA = validChars.toCharArray();
+	
+	for ( int i = 0; i < validCharsA.length; i++ )
+		if (letter == validCharsA[i] || letter == Character.toUpperCase(validCharsA[i]) ) {
+			System.out.println("Das ist kein Buchstabe!");
+		}
 	}
 
 }
