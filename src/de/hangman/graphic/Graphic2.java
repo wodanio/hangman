ce6cfd953783c13e0078f45531995caa9e7285e0
topssen <tobias.schnurrer@gmx.de>
2021-04-19 10:22:56 +0200

    graphic

--- src/de/hangman/graphic/Graphic2.java
+++ src/de/hangman/graphic/Graphic2.java
@@ -11,11 +11,12 @@ import javax.swing.JPanel;
 
 
 public class Graphic2 {
-	
+
 	private static ImageIcon showImg() {
-		BufferedImage img = null;
+		
+		BufferedImage img= null;
 		try {
-			img = ImageIO.read(new File("src/de/hangman/graphic/hangman9.png"));
+			img = ImageIO.read(new File("src/de/hangman/graphic/images/Hangman_1.jpeg"));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -35,7 +36,30 @@ public class Graphic2 {
 		JLabel label = new JLabel (showImg());
 		panel.add(label);
 		Fenster.add(panel);
+		
+		
+		String[] bilder = new String [12];
+		bilder [1] = "src/de/hangman/graphic/images/Hangman_1.jpeg";
+		bilder [2] = "src/de/hangman/graphic/images/Hangman_2.jpeg";
+		bilder [3] = "src/de/hangman/graphic/images/Hangman_3.jpeg";
+		bilder [4] = "src/de/hangman/graphic/images/Hangman_4.jpeg";
+		bilder [5] = "src/de/hangman/graphic/images/Hangman_5.jpeg";
+		bilder [6] = "src/de/hangman/graphic/images/Hangman_6.jpeg";
+		bilder [7] = "src/de/hangman/graphic/images/Hangman_7.jpeg";
+		bilder [8] = "src/de/hangman/graphic/images/Hangman_8.jpeg";
+		bilder [9] = "src/de/hangman/graphic/images/Hangman_9.jpeg";
+		bilder [10] = "src/de/hangman/graphic/images/Hangman_10.jpeg";
+		bilder [11] = "src/de/hangman/graphic/images/Hangman_11.jpeg";
+	
+	
+		img.setImageIcon(bilder);
+		
+		boolean eingabe = false;
+		if (eingabe = false) {
+			bilder = bilder ++;
 			
+		}
+					
 }
 	}
 
